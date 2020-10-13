6c7b9f62e9d6650e3a54fa0a7fd585c9366918dd
pudge-w <pudge_wj@163.com>
2020-10-13 15:42:38 +0800

    one piece

--- shangcheng/src/views/Home.vue
+++ shangcheng/src/views/Home.vue
@@ -2,10 +2,10 @@
   <div>
     <router-view></router-view>
     <van-tabbar v-model="active" active-color="orange" :placeholder="true">
-      <van-tabbar-item icon="wap-home-o" to="/index">首页</van-tabbar-item>
-      <van-tabbar-item icon="apps-o" to="/classify">分类</van-tabbar-item>
-      <van-tabbar-item icon="shopping-cart-o" to="/cart">购物车</van-tabbar-item>
-      <van-tabbar-item icon="contact" to="/mine">我的</van-tabbar-item>
+      <van-tabbar-item name="1" icon="wap-home-o" to="/index">首页</van-tabbar-item>
+      <van-tabbar-item name="2" icon="apps-o" to="/classify">分类</van-tabbar-item>
+      <van-tabbar-item name="3" icon="shopping-cart-o" to="/cart">购物车</van-tabbar-item>
+      <van-tabbar-item name="4" icon="contact" to="/mine">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
